1a6515b1a7982509d0b3bccd41c6d956a36cb57e
Aaron Amodt <aaron@amodttech.com>
2020-09-15 21:12:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,11 @@
 def square_array(array)
   array.each do |number|
     number**2
+    puts array
   end
 end
 
 
 
 
-brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
+=begin brothers.each{|brother| puts "Stop hitting yourself #{brother}!"} end=
