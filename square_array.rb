bb6c6b347ada7c4ad931816876e1604b08b10602
Aaron Amodt <aaron@amodttech.com>
2020-09-15 21:36:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,7 @@
-#def square_array(array)
-#    #numbers = []
-#    array.each do |elements|
-#      elements ** 2
-#    #numbers
-#  end
-#end
-
 def square_array(array)
-  squared = []
-  array.each { |element| squared << element ** 2 }
-  squared
+    #numbers = []
+    array.each do |elements|
+      elements ** 2
+    #numbers
+  end
 end
